e0c8d1d57dec6ae9c56b89fb4fb6a647807c5be5
StanEkso <90618842+StanEkso@users.noreply.github.com>
2022-10-15 22:55:10 +0300

    feat: validation for user dto

--- src/user/dto/userCreation.dto.ts
+++ src/user/dto/userCreation.dto.ts
@@ -1,5 +1,15 @@
+import { IsEmail, IsString, Length } from 'class-validator';
+
 export class UserCreationDto {
+  @IsEmail({}, { message: 'Field email must be email!' })
   email: string;
+
+  @IsString({ message: 'Password should be a string' })
+  @Length(4, 14, {
+    message: "Password length shouldn't be less than 4 and greater than 14",
+  })
   password: string;
+
+  @IsString({ message: 'Username is a string field' })
   username: string;
 }
